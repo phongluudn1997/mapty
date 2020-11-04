014bfaf16aabc74d6786fabf421aa45c4f683f91
Phong Luu <huynhpluu@gmail.com>
2020-11-04 23:41:09 +0700

    added: load leaflet map

--- script.js
+++ script.js
@@ -28,8 +28,18 @@ if (navigator.geolocation) {
     position => {
       console.log(position);
       const { latitude, longitude } = position.coords;
+      const coords = [latitude, longitude];
+      var map = L.map("map").setView(coords, 13);
 
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+        .openPopup();
     },
     () => {
       alert("Could not get your location");
